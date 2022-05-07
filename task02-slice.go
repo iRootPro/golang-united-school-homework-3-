98ac4961aedad6dd2cac4805fbf6d808275d2d07
Alexander Neupokoev <admin@armavir.ru>
2022-05-07 15:19:04 +0300

    task 02

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,12 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+
+	var copy_add = []int64{}
+	copy(input, copy_arr)
+
+	for i := len(input) - 1; i >= 0; i-- {
+		result = append(result, input[i])
+	}
 	return
 }
