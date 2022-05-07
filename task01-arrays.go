5b28aa343f68b9c411737003e68de5199ce093bb
Alexander Neupokoev <admin@armavir.ru>
2022-05-07 15:07:57 +0300

    task01

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,10 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+
+	for i := 0; i < 15; i++ {
+		result += input[i]
+	}
+
+	return result / 15
 }
