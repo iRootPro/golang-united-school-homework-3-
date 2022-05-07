696ad11969d6b5e24ffea0e7d74cac9e8e804236
Alexander Neupokoev <admin@armavir.ru>
2022-05-07 15:32:16 +0300

    fix task 03

--- task03-map.go
+++ task03-map.go
@@ -4,6 +4,7 @@ import "sort"
 
 func sortMapValues(input map[int]string) (result []string) {
 	keys := make([]int, 0, len(input))
+	result_arr := []string{}
 
 	for k := range input {
 		keys = append(keys, k)
@@ -12,7 +13,7 @@ func sortMapValues(input map[int]string) (result []string) {
 	sort.Ints(keys)
 
 	for _, k := range keys {
-		append(result, input[k])
+		append(result_arr, input[k])
 	}
-	return
+	return result_arr
 }
